d088993af6323802bd271919270a4859d8775396
Ben Sack <ben@sack.io>
2017-03-19 12:45:33 -0400

    api docs

---
+++ resources/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Wanderset API",
+  "version": "0.0.0",
+  "description": "RESTful JSON API for Wanderset Platform",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-15T17:35:12.253Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
