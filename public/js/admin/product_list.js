18b2188acb28a05c1a2bd4a1c7470a1760e5e2e7
Ben Sack <ben@sack.io>
2017-04-26 22:29:57 -0400

    adding pagination and searching to products

--- public/js/admin/product_list.js
+++ public/js/admin/product_list.js
@@ -24,14 +24,102 @@ var LoadProducts = function(options, callback){
         cb();
       });
     }
+  , function(cb){
+      _.extend(gb, a.o);
+      cb();
+    }
   ], function(err){
     a.cb(err, gb);
   });
 };
 
+var BuildQuery = function(options, callback){
+  var a = Belt.argulint(arguments)
+    , self = this
+    , gb = {};
+  a.o = _.defaults(a.o, {
+    'query': GB.criteria.query
+  });
+
+  var qry = {} //Belt.copy(a.o.query)
+    , val;
+
+  val = $('[name="_id"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\s/g, '');
+    qry['_id'] = val;
+  }
+
+  val = $('[name="name"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['name'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  val = $('[name="content"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['$or'] = [
+      {
+        'label.us': {
+          '$regex': val
+        , '$options': 'i'
+        }
+      }
+    , {
+        'description.us': {
+          '$regex': val
+        , '$options': 'i'
+        }
+      }
+    ];
+  }
+
+  val = $('[name="vendors"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['vendors'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  val = $('[name="categories"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['categories'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  val = $('[name="brands"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['brands'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  val = $('[name="options"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['options.values.us'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  return qry;
+};
+
 $(document).ready(function(){
   GB['criteria'] = _.defaults(queryObject.get() || {}, {
-    'limit': 500
+    'limit': 50
   , 'skip': 0
   , 'query': '{}'
   , 'sort': '{"_id": 1}'
@@ -39,6 +127,20 @@ $(document).ready(function(){
 
   GB.criteria.query = JSON.parse(GB.criteria.query);
   GB.criteria.sort = JSON.parse(GB.criteria.sort);
+  GB.criteria.skip = Belt.cast(GB.criteria.skip, 'number');
+  GB.criteria.limit = Belt.cast(GB.criteria.limit, 'number');
+
+  $(document).on('click', '[name="apply_filter"]', function(e){
+    e.preventDefault();
+
+    var qry = BuildQuery();
+
+    document.location = document.location.pathname
+    + '?limit=' + GB.criteria.limit
+    + '&skip=0'
+    + (GB.criteria.sort ? '&sort=' + encodeURIComponent(JSON.stringify(GB.criteria.sort)) : '')
+    + (qry ? '&query=' + encodeURIComponent(JSON.stringify(qry)) : '');
+  });
 
   $(document).on('click', '.product [name="delete"]', function(e){
     e.preventDefault();
@@ -65,9 +167,54 @@ $(document).ready(function(){
   LoadProducts(GB.criteria, function(err, res){
     if (err) return bootbox.alert(err.message);
 
+    $('[name="count"]').html((res.skip + 1) + '-' + Belt.cast(_.min([res.skip + res.limit, res.count]), 'string') + ' of ' + res.count);
+
     $('tbody').html(_.map(res.docs, function(d){
       d.options = d.options || {};
       return Templates.admin_product_list_row(d);
     }).join('\n'));
+
+    var phtml = '<div class="btn-group">';
+    for (var i = 1; (i - 1) * res.limit <= res.count; i++){
+      phtml += '<a href="' + document.location.pathname
+      + '?limit=' + res.limit
+      + '&skip=' + ((i - 1) * res.limit)
+      + (res.sort ? '&sort=' + encodeURIComponent(JSON.stringify(res.sort)) : '')
+      + (res.query ? '&query=' + encodeURIComponent(JSON.stringify(res.query)) : '')
+      + '" class="btn btn-'
+      + ((res.skip + 1) <= (res.limit * i)
+      && (res.skip + 1) > (res.limit * (i - 1))
+      ? 'primary' : 'default') + '">' + i + '</a>';
+    }
+    phtml += '</div>'
+    $('[name="pagination"]').html(phtml);
+
+    if (Belt.get(res.query, '_id')){
+      $('[name="_id"] [name="filter"]').val(res.query._id);
+    }
+
+    if (Belt.get(res.query, 'name.$regex')){
+      $('[name="name"] [name="filter"]').val(res.query.name.$regex.replace(/\\W\*/g, ' '));
+    }
+
+    if (Belt.get(res.query, 'vendors.$regex')){
+      $('[name="vendors"] [name="filter"]').val(res.query.vendors.$regex.replace(/\\W\*/g, ' '));
+    }
+
+    if (Belt.get(res.query, 'categories.$regex')){
+      $('[name="categories"] [name="filter"]').val(res.query.categories.$regex.replace(/\\W\*/g, ' '));
+    }
+
+    if (Belt.get(res.query, 'brands.$regex')){
+      $('[name="brands"] [name="filter"]').val(res.query.brands.$regex.replace(/\\W\*/g, ' '));
+    }
+
+    if (Belt.get(res.query, 'options.values.us.$regex')){
+      $('[name="options"] [name="filter"]').val(res.query.options.values.us.$regex.replace(/\\W\*/g, ' '));
+    }
+
+    if (Belt.get(res.query, '$or.0')){
+      $('[name="content"] [name="filter"]').val(res.query.$or[0]['label.us'].$regex.replace(/\\W\*/g, ' '));
+    }
   });
 });
