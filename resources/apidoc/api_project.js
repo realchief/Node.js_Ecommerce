a014ed547041bd9c1066089490e3c547a975b4b8
Ben Sack <ben@sack.io>
2017-05-03 15:41:08 -0400

    adding some dynamic code for products

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-03T19:39:58.201Z",
+    "time": "2017-05-03T19:41:01.249Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
