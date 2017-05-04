e0ea055285d7be15be56bea3e70ca071b4764d3f
Ben Sack <ben@sack.io>
2017-05-04 11:56:49 -0400

    latest

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-04T15:51:33.411Z",
+    "time": "2017-05-04T15:56:47.781Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
