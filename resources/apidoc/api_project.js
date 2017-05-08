6a8d91bd445366aea24efd069bba33b043e9feda
Ben Sack <ben@sack.io>
2017-05-08 09:54:02 -0400

    fixing media target positioning

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T12:55:39.980Z",
+    "time": "2017-05-08T13:54:01.672Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
