9013eb206894145e88e7c09e52632b0d6c87a2a5
Ben Sack <ben@sack.io>
2017-05-08 11:00:02 -0400

    added homepage

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T14:38:24.519Z",
+    "time": "2017-05-08T14:59:53.996Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
