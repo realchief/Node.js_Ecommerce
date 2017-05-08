926cc340297e58c18617fd8e590527de94642880
Ben Sack <ben@sack.io>
2017-05-08 14:29:04 -0400

    text shadows

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T18:13:27.017Z",
+    "time": "2017-05-08T18:29:02.918Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
