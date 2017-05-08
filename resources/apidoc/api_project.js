eefa54a12ecc8dc1b0e2c94ff67b69d1451b20fb
Ben Sack <ben@sack.io>
2017-05-08 10:38:30 -0400

    adding preview buttons

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T14:25:39.057Z",
+    "time": "2017-05-08T14:38:24.519Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
