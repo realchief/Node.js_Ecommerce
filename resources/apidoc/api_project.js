e903f03ffa80bac77ecb8e7341563f25dbe897a9
Ben Sack <ben@sack.io>
2017-05-08 14:09:47 -0400

    fixing target

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T18:08:32.172Z",
+    "time": "2017-05-08T18:09:45.849Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
