7bfd0c6fdb28232930b69dc63c1c66735159b98e
Ben Sack <ben@sack.io>
2017-05-18 13:30:34 -0400

    added install scripts

--- scripts/install-nginx.js
+++ scripts/install-nginx.js
@@ -1,1 +1,57 @@
-/home/ubuntu/wanderset-staging/node_modules/basecmd/scripts/install-nginx.js
+#!/usr/bin/env node
+
+var Path = require('path')
+  , Optionall = require('optionall')
+  , FS = require('fs')
+  , Async = require('async')
+  , _ = require('underscore')
+  , Belt = require('jsbelt')
+  , Util = require('util')
+  , Winston = require('winston')
+  , Events = require('events')
+  , Spinner = require('its-thinking')
+  , CP = require('child_process')
+  , Request = require('request')
+;
+
+var O = new Optionall({
+                       '__dirname': process.env.rootdir || process.cwd()
+                     , 'file_priority': [
+                         'package.json'
+                       , 'environment.json'
+                       , 'credentials.json'
+                       , 'config.json'
+                       ]
+                     });
+
+var Log = new Winston.Logger();
+Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timestamp': false});
+
+var Spin = new Spinner(4);
+
+var GB = _.defaults(O.argv, {
+  'template_path': Path.join(O.__dirname, '/node_modules/basecmd/resources/assets/nginx.conf.template')
+, 'config_path': Path.join(O.__dirname, '/resources/config/nginx.' + O.environment + '.conf')
+, 'nginx_path': '/etc/nginx/sites-enabled/' + O.domain
+, 'install_nginx_conf': true
+});
+
+GB = _.defaults(O.argv, {
+  'template': _.template(FS.readFileSync(GB.template_path).toString('utf8'))
+});
+
+GB['install_nginx_conf'] = Belt.cast(GB.install_nginx_conf, 'boolean');
+
+Spin.start();
+
+Async.waterfall([
+  function(cb){
+    FS.writeFileSync(GB.config_path, GB.template(O));
+    if (GB.install_nginx_conf) CP.execSync('sudo ln -sf "' + GB.config_path + '" "' + GB.nginx_path + '"');
+    return cb();
+  }
+], function(err){
+  Spin.stop();
+  if (err) Log.error(err);
+  return process.exit(err ? 1 : 0);
+});
