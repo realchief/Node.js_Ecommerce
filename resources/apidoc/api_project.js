bf72c065b9ca82c52228cd5f1f5a150b542a7f97
Ben Sack <ben@sack.io>
2017-05-30 13:09:39 -0400

    backdoor access

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-30T16:26:03.310Z",
+    "time": "2017-05-30T17:09:29.697Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
