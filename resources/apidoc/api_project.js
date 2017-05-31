8cefecaae89aea618ec37728e610af60b0730ca2
Ben Sack <ben@sack.io>
2017-05-30 20:56:20 -0400

    small hidings

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-31T00:51:01.757Z",
+    "time": "2017-05-31T00:56:16.942Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
