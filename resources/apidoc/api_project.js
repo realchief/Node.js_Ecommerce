a586c1a4d353b4ee475e2164bf7d5d819a5817dc
Ben Sack <ben@sack.io>
2017-05-31 09:24:50 -0400

    splitting into rows

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-31T13:22:16.554Z",
+    "time": "2017-05-31T13:24:43.492Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
