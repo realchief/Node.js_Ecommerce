b15336f1b1de698bfebb1b3efb69a25bd8b8f6f8
Ben Sack <ben@sack.io>
2017-07-06 09:12:29 -0400

    shopify updates

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-05T22:32:19.791Z",
+    "time": "2017-07-06T13:12:24.919Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
