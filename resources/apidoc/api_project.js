77c928f8fcbee363aeb2456c96c053842e55d11a
Ben Sack <ben@sack.io>
2017-07-12 15:27:37 -0400

    shopify sync in place

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-12T19:27:17.976Z",
+    "time": "2017-07-12T19:27:28.513Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
