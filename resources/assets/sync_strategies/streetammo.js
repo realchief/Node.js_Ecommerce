4f810f3db9c3f0fe8d5f93ecceb980ab104c0f6d
Ben Sack <ben@sack.io>
2017-07-17 09:02:19 -0400

    syncing error

---
+++ resources/assets/sync_strategies/streetammo.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var Path = require('path')
+  , Optionall = require('optionall')
+  , Async = require('async')
+  , _ = require('underscore')
+  , Belt = require('jsbelt')
+  , Winston = require('winston')
+  , Events = require('events')
+  , Moment = require('moment')
+  , Str = require('underscore.string')
+  , Querystring = require('querystring')
+  , Request = require('request')
+;
+
+module.exports = function(options){
+  var o = _.defaults(options || {}, {
+
+  });
+
+  var S = {};
+
+  return S;
+};
