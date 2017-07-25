72fce6e78d893da9e095f3de67b5488217f5125c
Ben Sack <ben@sack.io>
2017-07-25 11:48:33 -0400

    slicing sets for now

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-25T14:32:27.108Z",
+    "time": "2017-07-25T15:48:26.032Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
