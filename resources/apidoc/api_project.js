62bbd493911b61660898da875a41144767efa941
Ben Sack <ben@sack.io>
2017-07-26 08:45:23 -0400

    updating homepage

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-26T12:44:08.523Z",
+    "time": "2017-07-26T12:45:12.713Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
