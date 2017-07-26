caa9da79cf90eacf211871f88e8294912e49bfc2
Ben Sack <ben@sack.io>
2017-07-25 20:10:28 -0400

    add logout

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-25T23:25:58.001Z",
+    "time": "2017-07-26T00:10:27.863Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
