6fa6d527bc5d1eb8298710bd4a389e06f7d90422
Ben Sack <ben@sack.io>
2017-07-26 00:56:22 -0400

    downsampling set images

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-26T04:43:09.927Z",
+    "time": "2017-07-26T04:56:14.684Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
--- scripts/update-categories-csv.js
+++ scripts/update-categories-csv.js
@@ -33,7 +33,7 @@ Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timest
 var Spin = new Spinner(4);
 
 var GB = _.defaults(O.argv, {
-  'csv_path': '/home/ben/Downloads/Wanderset Product Categories v2.csv'
+  'csv_path': '/home/ben/Downloads/Wanderset Product Categories - Alec v2 - Sheet1 (1).csv'
 , 'category_fields': [
     'Cat 1'
   , 'Cat 2'
