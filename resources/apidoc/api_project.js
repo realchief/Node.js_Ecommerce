47b65a58636d39fe25fda3619560a99735771e11
Ben Sack <ben@sack.io>
2017-07-29 11:05:33 -0400

    adding email public files

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-28T20:14:48.038Z",
+    "time": "2017-07-29T15:05:24.249Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
