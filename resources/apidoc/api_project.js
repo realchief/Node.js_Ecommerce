a8411de83c531a5ad3edc104a4572121013efaea
Ben Sack <ben@sack.io>
2017-07-29 18:47:49 -0400

    muting errors on sync

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-29T22:42:24.006Z",
+    "time": "2017-07-29T22:47:40.766Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
