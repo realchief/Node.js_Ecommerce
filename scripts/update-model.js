984f98fc48dfac5f10fc331ffbab661513c27ee7
Ben Sack <ben@sack.io>
2017-07-30 14:06:06 -0400

    admin lockdown

---
+++ scripts/update-model.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+var Path = require('path')
+  , Optionall = require('optionall')
+  , FS = require('fs')
+  , Async = require('async')
+  , _ = require('underscore')
+  , Belt = require('jsbelt')
+  , Util = require('util')
+  , Winston = require('winston')
+  , Events = require('events')
+  , Spinner = require('its-thinking')
+  , CP = require('child_process')
+  , Request = require('request')
+  , Assert = require('assert')
+  , CSV = require('fast-csv')
+;
+
+var O = new Optionall({
+                       '__dirname': Path.resolve(module.filename + '/../..')
+                     , 'file_priority': [
+                         'package.json'
+                       , 'environment.json'
+                       , 'settings.json'
+                       , 'config.json'
+                       ]
+                     });
+
+var Log = new Winston.Logger();
+Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timestamp': false});
+
+var Spin = new Spinner(4);
+
+var GB = _.defaults(O.argv, {
+  'query': {
+
+  }
+, 'skip': 0
+, 'limit': 1
+, 'auth': {
+    'user': 'wanderset'
+  , 'pass': 'wanderset1234'
+  }
+, 'model': 'vendor'
+, 'iterator': function(o, cb){
+    console.log('Updating ' + GB.model + ' [' + o._id + ']...');
+
+    Request({
+      'url': O.host + '/' + GB.model + '/' + o._id + '/update.json'
+    , 'auth': GB.auth
+    , 'body': {
+
+      }
+    , 'json': true
+    , 'method': 'post'
+    }, Belt.cw(cb));
+  }
+});
+
+Spin.start();
+
+Async.waterfall([
+  function(cb){
+    var cont;
+
+    return Async.doWhilst(function(next){
+      Request({
+        'url': O.host + '/' + GB.model + '/list.json'
+      , 'auth': GB.auth
+      , 'qs': {
+          'query': GB.query
+        , 'skip': GB.skip
+        , 'limit': GB.limit
+        }
+      , 'method': 'get'
+      , 'json': true
+      }, function(err, res, json){
+        cont = _.any(Belt.get(json, 'data')) ? true : false;
+        GB.skip += GB.limit;
+        console.log(GB.skip);
+
+        Async.eachLimit(Belt.get(json, 'data') || [], 6, function(d, cb2){
+          GB.iterator(d, cb2);
+        }, Belt.cw(next, 0));
+      })
+    }, function(){ return cont; }, Belt.cw(cb, 0));
+  }
+], function(err){
+  Spin.stop();
+  if (err) Log.error(err);
+  return process.exit(err ? 1 : 0);
+});
