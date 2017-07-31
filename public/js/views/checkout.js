e8e50b9cebacdce167ed105ccd4b22bf6b692e97
Ben Sack <ben@sack.io>
2017-07-30 23:07:36 -0400

    working on checkout

--- public/js/views/checkout.js
+++ public/js/views/checkout.js
@@ -70,25 +70,6 @@ var CheckoutView = function(options, callback){
   return gb.view;
 };
 
-$(document).on('click', '[name="cart_product_remove"]', function(e){
-  e.preventDefault();
-
-  $.getJSON($(this).attr('href'), function(){
-    document.location.reload();
-  });
-});
-
-$(document).on('click', '[name="cart_product_save"]', function(e){
-  e.preventDefault();
-
-  var prod = $(this).parents('[data-view="BagProductView"], [data-view="BagProductMobileView"]')
-    , qty = prod.find('[data-get="quantity"]').val();
-
-  $.getJSON('/cart/session/product/' + prod.attr('data-id') + '/quantity/' + qty + '/update.json', function(){
-    document.location.reload();
-  });
-});
-
 $(document).ready(function(){
   GB['view'] = new CheckoutView({
 
