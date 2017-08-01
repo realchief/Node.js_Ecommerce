006b89268cc6103bd95754cc5e050fbf3db73e13
Ben Sack <ben@sack.io>
2017-08-01 11:13:26 -0400

    sharing links enabled

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-01T15:11:20.364Z",
+    "time": "2017-08-01T15:13:16.867Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
