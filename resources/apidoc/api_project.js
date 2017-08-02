4821c5687a7b5a09127f980f49e4416d7910a811
Ben Sack <ben@sack.io>
2017-08-02 12:37:43 -0400

    hide featured set on homepage

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-02T16:26:09.712Z",
+    "time": "2017-08-02T16:37:36.409Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
