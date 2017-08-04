4688a41215bf74114f39cb3fe60e9f5fc487b38b
Ben Sack <ben@sack.io>
2017-08-04 12:14:01 -0400

    changes to ordering and stock outs

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-04T16:08:52.207Z",
+    "time": "2017-08-04T16:14:00.950Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
