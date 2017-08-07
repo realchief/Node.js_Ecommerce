df754e460f6a0bcc0b9b52caf3589fecf8dd4898
Ben Sack <ben@sack.io>
2017-08-07 13:12:39 -0400

    preserving set order for shopify sync

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-07T16:50:26.440Z",
+    "time": "2017-08-07T17:12:28.165Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
