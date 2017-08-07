38284a45aec567cc76e1a1685b31480c667e50da
Ben Sack <ben@sack.io>
2017-08-07 09:13:46 -0400

    reducing set pagination elements

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-07T13:13:01.028Z",
+    "time": "2017-08-07T13:13:44.424Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
