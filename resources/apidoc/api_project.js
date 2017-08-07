64cf1c8477772b950b9d2c8b7c63e049edb8cffe
Ben Sack <ben@sack.io>
2017-08-07 11:34:50 -0400

    pagination fix

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-07T15:07:33.820Z",
+    "time": "2017-08-07T15:34:38.351Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
