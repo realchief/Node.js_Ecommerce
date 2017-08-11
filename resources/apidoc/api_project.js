afa936029eed72318956fa874e93fba8c07c4a44
Ben Sack <ben@sack.io>
2017-08-11 08:21:04 -0400

    order confirmation

--- resources/apidoc/api_project.js
+++ resources/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-10T21:15:05.968Z",
+    "time": "2017-08-11T12:21:00.232Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
