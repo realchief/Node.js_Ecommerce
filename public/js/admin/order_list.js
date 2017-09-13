d1838206e61a2271962265c5c2b9498b426a6c31
Ben Sack <ben@sack.io>
2017-09-13 12:43:42 -0400

    progress on order management

---
+++ public/js/admin/order_list.js
@@ -0,0 +1,148 @@
+var LoadDocs = function(options, callback){
+  var a = Belt.argulint(arguments)
+    , self = this
+    , gb = {};
+  a.o = _.defaults(a.o, {
+    'skip': 0
+  , 'limit': 100
+  });
+
+  return Async.waterfall([
+    function(cb){
+      $.post('/' + GB.model + '/count.json', a.o, function(json){
+        if (Belt.get(json, 'error')) return cb(new Error(json.error));
+
+        gb['count'] = Belt.get(json, 'data');
+        cb();
+      });
+    }
+  , function(cb){
+      $.post('/' + GB.model + '/list.json', a.o, function(json){
+        if (Belt.get(json, 'error')) return cb(new Error(json.error));
+
+        gb['docs'] = Belt.get(json, 'data');
+        cb();
+      });
+    }
+  , function(cb){
+      _.extend(gb, a.o);
+      cb();
+    }
+  ], function(err){
+    a.cb(err, gb);
+  });
+};
+
+var BuildQuery = function(options, callback){
+  var a = Belt.argulint(arguments)
+    , self = this
+    , gb = {};
+  a.o = _.defaults(a.o, {
+    'query': GB.criteria.query
+  });
+
+  var qry = {} //Belt.copy(a.o.query)
+    , val;
+
+  val = $('[name="_id"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\s/g, '');
+    qry['_id'] = val;
+  }
+
+  val = $('[name="name"] [name="filter"]').val();
+  if (val){
+    val = val.replace(/\W+/g, '\\W*');
+    qry['name'] = {
+      '$regex': val
+    , '$options': 'i'
+    };
+  }
+
+  return qry;
+};
+
+$(document).ready(function(){
+  GB['model'] = 'order';
+
+  GB['criteria'] = _.defaults(queryObject.get() || {}, {
+    'limit': 50
+  , 'skip': 0
+  , 'query': '{}'
+  , 'sort': '{"_id": 1}'
+  });
+
+  GB.criteria.query = JSON.parse(GB.criteria.query);
+  GB.criteria.sort = JSON.parse(GB.criteria.sort);
+  GB.criteria.skip = Belt.cast(GB.criteria.skip, 'number');
+  GB.criteria.limit = Belt.cast(GB.criteria.limit, 'number');
+
+  $(document).on('click', '[name="apply_filter"]', function(e){
+    e.preventDefault();
+
+    var qry = BuildQuery();
+
+    document.location = document.location.pathname
+    + '?limit=' + GB.criteria.limit
+    + '&skip=0'
+    + (GB.criteria.sort ? '&sort=' + encodeURIComponent(JSON.stringify(GB.criteria.sort)) : '')
+    + (qry ? '&query=' + encodeURIComponent(JSON.stringify(qry)) : '');
+  });
+
+  $(document).on('click', '.' + GB.model + ' [name="delete"]', function(e){
+    e.preventDefault();
+
+    var $doc = $(this).parents('.' + GB.model)
+      , id = $doc.attr('data-id');
+
+    bootbox.confirm('Are you sure you want to delete this ' + GB.model + '?', function(yes){
+      if (!yes) return;
+
+      $.ajax({
+        'url': '/' + GB.model + '/' + id + '/delete.json'
+      , 'type': 'DELETE'
+      , 'dataType': 'json'
+      , 'success': function(json){
+          if (Belt.get(json, 'error')) return bootbox.alert(json.error);
+
+          $doc.remove();
+        }
+      })
+    });
+  });
+
+  LoadDocs(GB.criteria, function(err, res){
+    if (err) return bootbox.alert(err.message);
+
+    $('[name="count"]').html((res.skip + 1) + '-' + Belt.cast(_.min([res.skip + res.limit, res.count]), 'string') + ' of ' + res.count);
+
+    $('tbody').html(_.map(res.docs, function(d){
+      d.options = d.options || {};
+      d.Instance = Instance;
+      return Templates['admin_' + GB.model + '_list_row'](d);
+    }).join('\n'));
+
+    var phtml = '<div class="btn-group">';
+    for (var i = 1; (i - 1) * res.limit <= res.count; i++){
+      phtml += '<a href="' + document.location.pathname
+      + '?limit=' + res.limit
+      + '&skip=' + ((i - 1) * res.limit)
+      + (res.sort ? '&sort=' + encodeURIComponent(JSON.stringify(res.sort)) : '')
+      + (res.query ? '&query=' + encodeURIComponent(JSON.stringify(res.query)) : '')
+      + '" class="btn btn-'
+      + ((res.skip + 1) <= (res.limit * i)
+      && (res.skip + 1) > (res.limit * (i - 1))
+      ? 'primary' : 'default') + '">' + i + '</a>';
+    }
+    phtml += '</div>'
+    $('[name="pagination"]').html(phtml);
+
+    if (Belt.get(res.query, '_id')){
+      $('[name="_id"] [name="filter"]').val(res.query._id);
+    }
+
+    if (Belt.get(res.query, '$or.0')){
+      $('[name="content"] [name="filter"]').val(res.query.$or[0]['label.us'].$regex.replace(/\\W\*/g, ' '));
+    }
+  });
+});
---
+++ scripts/get-shopify-order.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+
+var Path = require('path')
+  , Optionall = require('optionall')
+  , FS = require('fs')
+  , Async = require('async')
+  , _ = require('underscore')
+  , Belt = require('jsbelt')
+  , Util = require('util')
+  , Winston = require('winston')
+  , Events = require('events')
+  , Spinner = require('its-thinking')
+  , CP = require('child_process')
+  , Request = require('request')
+  , Assert = require('assert')
+  , CSV = require('fast-csv')
+  , Shopify = require('shopify-node-api')
+;
+
+var O = new Optionall({
+                       '__dirname': Path.resolve(module.filename + '/../..')
+                     , 'file_priority': [
+                         'package.json'
+                       , 'environment.json'
+                       , 'settings.json'
+                       , 'config.json'
+                       ]
+                     });
+
+var Log = new Winston.Logger();
+Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timestamp': false});
+
+var Spin = new Spinner(4);
+
+var GB = _.defaults(O.argv, {
+  'query': Belt.stringify({
+    '_id': '596404a139f4554967999401'
+  })
+, 'skip': 0
+, 'limit': 1
+, 'auth': {
+    'user': _.keys(O.admin_users)[0]
+  , 'pass': _.values(O.admin_users)[0]
+  }
+, 'model': 'vendor'
+, 'order': {
+billing_address: {
+first_name: "greg",
+last_name: "selkoe",
+address1: "400 Stuart Street",
+address2: "19D",
+city: "boston",
+province: "MA",
+country: "US",
+zip: "02116",
+phone: "617-216-1013"
+},
+shipping_address: {
+first_name: "greg",
+last_name: "selkoe",
+address1: "1182 Sunset Hills Rd",
+address2: "",
+city: "West Hollywood",
+province: "CA",
+country: "US",
+zip: "90006",
+phone: "6172161013"
+},
+note: "wanderset dropship order #SJUDR2N5Z",
+phone: "6173000585",
+buyer_accepts_marketing: false,
+financial_status: "authorized",
+tags: "wanderset",
+email: "orders@wanderset.com",
+line_items: [
+{
+product_id: 9148811337,
+quantity: 1,
+price: "95.00",
+variant_id: 33510250505
+}
+],
+total_price: "95.00"
+  }
+, 'iterator': function(o, cb){
+    var shopify = new Shopify({
+      'shop': o.shopify.shop
+    , 'shopify_api_key': O.shopify.key
+    , 'access_token': o.shopify.access_token
+    });
+
+    shopify.post('/admin/orders.json', {
+      'order': GB.order
+    }, function(err, data){
+      console.log(Belt.stringify(arguments));
+
+      console.log(o.name + ' order ' + Belt.get(data, 'order.id'));
+
+      cb();
+    });
+  }
+});
+
+Spin.start();
+
+Async.waterfall([
+  function(cb){
+    var cont;
+
+    return Async.doWhilst(function(next){
+      Request({
+        'url': O.host + '/admin/' + GB.model + '/list.json'
+      , 'auth': GB.auth
+      , 'qs': {
+          'query': GB.query
+        , 'skip': GB.skip
+        , 'limit': GB.limit
+        }
+      , 'method': 'get'
+      , 'json': true
+      }, function(err, res, json){
+        cont = _.any(Belt.get(json, 'data')) ? true : false;
+        GB.skip += GB.limit;
+        console.log(GB.skip);
+
+        Async.eachLimit(Belt.get(json, 'data') || [], 6, function(d, cb2){
+          GB.iterator(d, cb2);
+        }, Belt.cw(next, 0));
+      })
+    }, function(){ return cont; }, Belt.cw(cb, 0));
+  }
+], function(err){
+  Spin.stop();
+  if (err) Log.error(err);
+  return process.exit(err ? 1 : 0);
+});
