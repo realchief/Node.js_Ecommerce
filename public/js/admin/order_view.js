4a51af292c05ec006d99b1115dffb849176559b8
Ben Sack <ben@sack.io>
2017-10-02 11:17:27 -0400

    fixing admin add shipment

--- public/js/admin/order_view.js
+++ public/js/admin/order_view.js
@@ -17,6 +17,7 @@ var OrderView = function(options, callback){
     }
   , 'transformers': {
       'set:products': function(val){
+console.log(val)
         return _.map(val, function(v){
           return '<option value="' + v._id + '">' + Belt.get(v, 'source.product.label.us') + ' - ' + v._id + '</option>';
         }).join('\n')
@@ -99,7 +100,7 @@ var OrderView = function(options, callback){
     $.post('/admin/order/' + self._id + '/shipment/create.json', {
       'shipment': gb
     }, function(res){
-      ('input, select').val('');
+      $('input, select').val('');
 
       LoadDocs(GB.criteria, function(err, res){
         if (err) return bootbox.alert(err.message);
