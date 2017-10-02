87b0d345cec455c00b6267c677c06db24c487585
Ben Sack <ben@sack.io>
2017-10-01 20:17:28 -0400

    adding ability to create shipments manually

---
+++ scripts/create-order-shipment.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+var Path = require('path')
+  , Optionall = require('optionall')
+  , FS = require('fs')
+  , Async = require('async')
+  , _ = require('underscore')
+  , Belt = require('jsbelt')
+  , Util = require('util')
+  , Winston = require('winston')
+  , Events = require('events')
+  , Spinner = require('its-thinking')
+  , CP = require('child_process')
+  , Request = require('request')
+  , Assert = require('assert')
+  , CSV = require('fast-csv')
+;
+
+var O = new Optionall({
+                       '__dirname': Path.resolve(module.filename + '/../..')
+                     , 'file_priority': [
+                         'package.json'
+                       , 'environment.json'
+                       , 'settings.json'
+                       , 'config.json'
+                       ]
+                     });
+
+var Log = new Winston.Logger();
+Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timestamp': false});
+
+var Spin = new Spinner(4);
+
+var GB = _.defaults(O.argv, {
+  'query': {
+    'slug': 'B1CX5Z9DW'
+  }
+, 'skip': 0
+, 'limit': 1
+, 'auth': {
+    'user': _.keys(O.admin_users)[0]
+  , 'pass': _.values(O.admin_users)[0]
+  }
+, 'model': 'order'
+, 'iterator': function(o, cb){
+    console.log('Updating ' + GB.model + ' [' + o._id + ']...');
+
+    Request({
+      'url': O.host + '/admin/' + GB.model + '/' + o.slug + '/shipment/create.json'
+    , 'auth': GB.auth
+    , 'body': {
+        'shipment': {
+          'created_at': new Date()
+        , 'id': '787488631462'
+        , 'carrier': 'Fedex'
+        , 'tracking_number':'787488631462'
+        , 'tracking_url': 'https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=787488631462&locale=en_US&cntry_code=us'
+        , 'products': [
+            '599c99f4e4834ed1c4f1027d'
+          ]
+        , 'status': 'delivered'
+        , 'sent_shipped_email': false
+        }
+      }
+    , 'json': true
+    , 'method': 'post'
+    }, Belt.cw(cb));
+  }
+});
+
+Spin.start();
+
+Async.waterfall([
+  function(cb){
+    var cont;
+
+    return Async.doWhilst(function(next){
+      Request({
+        'url': O.host + '/' + GB.model + '/list.json'
+      , 'auth': GB.auth
+      , 'qs': {
+          'query': GB.query
+        , 'skip': GB.skip
+        , 'limit': GB.limit
+        }
+      , 'method': 'get'
+      , 'json': true
+      }, function(err, res, json){
+        cont = _.any(Belt.get(json, 'data')) ? true : false;
+        GB.skip += GB.limit;
+        console.log(GB.skip);
+
+        Async.eachLimit(Belt.get(json, 'data') || [], 6, function(d, cb2){
+          GB.iterator(d, cb2);
+        }, Belt.cw(next, 0));
+      })
+    }, function(){ return cont; }, Belt.cw(cb, 0));
+  }
+], function(err){
+  Spin.stop();
+  if (err) Log.error(err);
+  return process.exit(err ? 1 : 0);
+});
