c0c2bc08d96aeae3cb601607d8e508a642fd7988
Ben Sack <ben@sack.io>
2017-11-27 12:31:23 -0500

    logging out on error

--- scripts/update-promo-code.js
+++ scripts/update-promo-code.js
@@ -34,7 +34,7 @@ var Spin = new Spinner(4);
 
 var GB = _.defaults(O.argv, {
   'query': {
-    'code': 'cyber20'
+    'code': 'krtsloaded100stuffedturkey'
   }
 , 'skip': 0
 , 'limit': 100
@@ -50,7 +50,7 @@ var GB = _.defaults(O.argv, {
       'url': O.host + '/admin/' + GB.model + '/' + o._id + '/update.json'
     , 'auth': GB.auth
     , 'body': {
-        'label': '20% Off'
+        'code': 'krtsloaded100krtsmon'
       }
     , 'json': true
     , 'method': 'post'
