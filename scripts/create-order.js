c979daccf14bf6e095534c30a1bb45c558c4c184
Ben Sack <ben@sack.io>
2017-12-27 23:16:33 -0500

    fixes

--- scripts/create-order.js
+++ scripts/create-order.js
@@ -33,10 +33,22 @@ Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timest
 var Spin = new Spinner(4);
 
 var GB = _.defaults(O.argv, {
-  'cart': '5a246746c9bea008f0519a2e'
+  'cart': '5a442d1ff57b36091cb614ab'
 , 'query': {
-    'customer_id': 'cus_BsqUwL4n0ni8i8'
-  , 'payment_method': 'card_1BV4nOBqgBRCGoCvg15vTFpa'
+//    'customer_id': 'cus_C1ubarRLDKZeFR'
+//  , 'payment_method': 'card_1Bdqm2BqgBRCGoCvJygabamk'
+    'transaction': {
+      'id': '50K088727C7085630'
+    , 'amount': 87.20
+    , 'type': 'charge'
+    , 'description': 'charged at paypal checkout'
+    , 'source': {
+        'id': '6V489904MJ331953X'
+      }
+    , 'payment_gateway': 'paypal'
+    , 'amount_refunded': 0
+    , 'status': 'paid'
+    }
   }
 , 'auth': {
     'user': _.keys(O.admin_users)[0]
