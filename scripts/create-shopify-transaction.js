f63121cdf0232cee5f0eaea7862f732064b29d18
Ben Sack <ben@sack.io>
2018-01-17 13:05:41 -0500

    fixing active ride shop

--- scripts/create-shopify-transaction.js
+++ scripts/create-shopify-transaction.js
@@ -12,7 +12,9 @@ var Path = require('path')
   , Spinner = require('its-thinking')
   , CP = require('child_process')
   , Request = require('request')
+  , Assert = require('assert')
   , CSV = require('fast-csv')
+  , Shopify = require('shopify-node-api')
 ;
 
 var O = new Optionall({
@@ -31,27 +33,38 @@ Log.add(Winston.transports.Console, {'level': 'debug', 'colorize': true, 'timest
 var Spin = new Spinner(4);
 
 var GB = _.defaults(O.argv, {
-  'query': {
-
-  }
-, 'set': 'on-sale'
+  'query': Belt.stringify({
+    '_id': '5a46c1f0cf845a091aa5fc02'
+  })
 , 'skip': 0
-, 'limit': 100
+, 'limit': 1
 , 'auth': {
     'user': _.keys(O.admin_users)[0]
   , 'pass': _.values(O.admin_users)[0]
   }
+, 'model': 'vendor'
+, 'order': '22153055 4377'
+, 'transaction': {
+    'amount': '37.99'
+  , 'authorization': Belt.uuid()
+  , 'kind': 'authorization'
+  , 'gateway': 'bogus'
+  , 'currency': 'USD'
+  , 'status': 'success'
+  , 'source_name': 'wanderset'
+  }
 , 'iterator': function(o, cb){
-    if (!o.compare_at_price || !o.low_price || o.compare_at_price <= o.low_price) return cb();
+    var shopify = new Shopify({
+      'shop': o.shopify.shop
+    , 'shopify_api_key': O.shopify.key
+    , 'access_token': o.shopify.access_token
+    });
 
-    console.log('Adding "' + o.slug + '"...');
+    shopify.post('/admin/orders/' + GB.order + '/transactions.json', {
+      'transaction': GB.transaction
+    }, function(err, data){
+      console.log(Belt.stringify(arguments));
 
-    Request({
-      'url': O.host + '/set/' + GB.set + '/product/' + o._id + '/add.json'
-    , 'auth': GB.auth
-    , 'method': 'post'
-    , 'json': true
-    }, function(err, res, json){
       cb();
     });
   }
@@ -59,15 +72,13 @@ var GB = _.defaults(O.argv, {
 
 Spin.start();
 
-O.host = 'http://wanderset.com:8852';
-
 Async.waterfall([
   function(cb){
     var cont;
 
     return Async.doWhilst(function(next){
       Request({
-        'url': O.host + '/product/list.json'
+        'url': O.host + '/admin/' + GB.model + '/list.json'
       , 'auth': GB.auth
       , 'qs': {
           'query': GB.query
@@ -79,8 +90,9 @@ Async.waterfall([
       }, function(err, res, json){
         cont = _.any(Belt.get(json, 'data')) ? true : false;
         GB.skip += GB.limit;
+        console.log(GB.skip);
 
-        Async.eachSeries(Belt.get(json, 'data') || [], function(d, cb2){
+        Async.eachLimit(Belt.get(json, 'data') || [], 6, function(d, cb2){
           GB.iterator(d, cb2);
         }, Belt.cw(next, 0));
       })
