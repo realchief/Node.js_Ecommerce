88b802a746c7072d8a3f04f088fa55d74473f330
Zvonimir Sabljic <zvonimirsabljic@Zvonimirs-MacBook-Pro-2.local>
2018-03-24 16:52:37 +0100

    Bring back blazy firing on sets and product lists

--- public/js/views/product_list.js
+++ public/js/views/product_list.js
@@ -127,6 +127,16 @@ var LoadProducts = function(options, callback){
   ], function(err){
     ToggleFooterLoader();
 
+    var checkLoadedImages = function () {
+        var elements = $('.tab-content img.b-lazy');
+        if (elements.length === 0) elements = $('img.b-lazy');
+        if (elements && elements.length > 0 && !_.any(elements, function (el) { return el.className.indexOf('b-loaded') !== -1; })) {
+            var bLazy = new Blazy();
+            setTimeout(checkLoadedImages, 500);
+        }
+    };
+    checkLoadedImages();
+
     a.cb(err, gb.data);
   });
 };
